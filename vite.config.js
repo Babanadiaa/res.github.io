54e4af0fd6811148c265e9877beaa6c74974a216
Maksym Atamanchuk <maksumatmn@gmail.com>
2025-03-04 15:20:06 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
   plugins:
     [react(), tailwindcss()],
   base: "/Babanadiaa.github.io"
