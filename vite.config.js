f8e267f2255018ded94976376062ac51b823a895
Maksym Atamanchuk <maksumatmn@gmail.com>
2025-03-05 13:26:22 +0200

    restart

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
-  plugins: [react()],
+  plugins: [react(), tailwindcss(),],
   base: '/Babanadiaa.github.io/',
 })
